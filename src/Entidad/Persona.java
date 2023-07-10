61fdbd12822694e068079e961466cb81c5b01d9a
D1360 <d13604f6@gmail.com>
2023-07-09 20:18:17 -0500

    primer commit

---
+++ src/Entidad/Persona.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Entidad;
+
+/**
+ *
+ * @author d1360
+ */
+public class Persona {
+    
+    public String nombre;
+    
+    public Persona(){
+    }    
+    public Persona(String nombre ) {
+    this.nombre = nombre;
+    
+    }   
+}
